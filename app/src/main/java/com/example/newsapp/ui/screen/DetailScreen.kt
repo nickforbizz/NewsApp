cc85010edacc08a67a415328f31b8ccaf0612825
nwaruingi <nwaruingi@kemri-wellcome.org>
2022-06-24 05:48:00 +0300

    navigation

--- app/src/main/java/com/example/newsapp/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screen/DetailScreen.kt
@@ -2,17 +2,26 @@ package com.example.newsapp.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun DetailScreen(){
+fun DetailScreen(navController: NavController){
     Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "Details Screen", fontWeight = FontWeight.SemiBold)
+        Button(onClick = {
+            //navController.navigate("TopNews")
+            navController.popBackStack()
+        }) {
+            Text(text = "Go To Top News")
+        }
     }
 }
 
@@ -21,5 +30,5 @@ fun DetailScreen(){
 @Preview(showBackground = true)
 @Composable
 fun DetailScreenPreview(){
-    DetailScreen()
+    DetailScreen(rememberNavController())
 }
--- app/src/main/java/com/example/newsapp/ui/screen/TopNews.kt
+++ app/src/main/java/com/example/newsapp/ui/screen/TopNews.kt
@@ -2,17 +2,25 @@ package com.example.newsapp.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun TopNews(){
+fun TopNews(navController: NavController){
     Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "Top News", fontWeight = FontWeight.SemiBold)
+        Button(onClick = {
+            navController.navigate("DetailScreen")
+        }) {
+            Text(text = "Go To Detail Screen")
+        }
     }
 }
 
@@ -21,5 +29,5 @@ fun TopNews(){
 @Preview(showBackground = true)
 @Composable
 fun TopNewsPreview(){
-    TopNews()
+    TopNews(rememberNavController())
 }
