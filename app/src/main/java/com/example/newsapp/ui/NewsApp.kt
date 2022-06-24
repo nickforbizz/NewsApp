8d2497241a535a564dfa25fd7e8928e4a30d798c
nwaruingi <nwaruingi@kemri-wellcome.org>
2022-06-24 04:57:09 +0300

    first commit

---
+++ app/src/main/java/com/example/newsapp/ui/NewsApp.kt
@@ -0,0 +1,14 @@
+package com.example.newsapp.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NewsApp(){
+    Navigation()
+}
+
+
+@Composable
+fun Navigation(){
+
+}
---
+++ app/src/main/java/com/example/newsapp/ui/screen/DetailScreen.kt
@@ -0,0 +1,25 @@
+package com.example.newsapp.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DetailScreen(){
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Details Screen", fontWeight = FontWeight.SemiBold)
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview(){
+    DetailScreen()
+}
