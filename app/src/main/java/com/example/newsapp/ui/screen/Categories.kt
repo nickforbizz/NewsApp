4bb8c53874d452a4331becb94e43558af701d0e9
nwaruingi <nwaruingi@kemri-wellcome.org>
2022-07-10 12:25:22 +0300

    bottom bar and navigation with composables

---
+++ app/src/main/java/com/example/newsapp/ui/screen/Categories.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.example.newsapp.BottomMenuScreen
+
+@Composable
+fun Categories(){
+    Text(text = "Category Screen")
+}
---
+++ app/src/main/java/com/example/newsapp/ui/screen/Sources.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.ui.screen
+
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.example.newsapp.BottomMenuScreen
+
+@Composable
+fun Sources(){
+    Text(text = "Sources Screen")
+}
