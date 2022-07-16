6ca04e53d85f1255de4b6342d430f749447a7a4d
nwaruingi <nwaruingi@kemri-wellcome.org>
2022-07-16 14:14:15 +0300

    Retrofit Api endpoint v1

---
+++ app/src/main/java/com/example/newsapp/network/NewsService.kt
@@ -0,0 +1,15 @@
+package com.example.newsapp.network
+
+import com.example.newsapp.models.TopNewsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsService {
+
+    @GET("top-headlines")
+    fun getTopArticles(
+        @Query("country") country:String,
+        @Query("apiKey") apiKey:String,
+    ): Call<TopNewsResponse>
+}
